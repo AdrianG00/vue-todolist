3cc283050674d21d72e348857f14925a5c4c0e33
AdrianG00 <giurgicatalin@gmail.com>
2022-05-10 09:40:05 +0200

    Add: To do list script

---
+++ js/script.js
@@ -0,0 +1,48 @@
+
+const app = new Vue({
+  el: '#app',
+  data:{
+    lists:[
+      {
+        text: 'Comprare pane',
+        done: false
+      },
+      {
+        text: 'Comprare olio auto',
+        done: true
+      },
+      {
+        text: 'Pagare multa',
+        done: false
+      }
+    ],
+    newList: ''
+  },
+
+  methods:{
+    
+    addList(){
+      
+      if(this.newList.length > 1) {
+        
+        const newListPush = {
+          text: this.newList,
+          done: false
+        }
+        this.lists.push(newListPush);
+        this.newList = '';
+      }   
+    },
+
+    removeList(index){
+
+      if(confirm(`Sicuro di voler eliminare l'elemento dalla lista?S`)){
+        this.lists.splice(index, 1);
+      }
+    },
+
+    toggleDone(index){
+      this.lists[index].done = !this.lists[index].done;
+    }
+  }
+})
